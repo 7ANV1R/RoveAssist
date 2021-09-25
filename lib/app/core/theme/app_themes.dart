785163d5ea2f05bb7fae4bb8e8b8bf5b7ee77596
Tanvir <ibnmizan7878@gmail.com>
2021-09-26 04:22:19 +0600

    updated theme

--- lib/app/core/theme/app_themes.dart
+++ lib/app/core/theme/app_themes.dart
@@ -10,9 +10,9 @@ List<Map<String, dynamic>> themes = [
     'brightness': Brightness.light,
     'data': ThemeData(
       fontFamily: getFontFamilyFromLocale(),
-      primaryColor: const Color(0xff0054BF),
-      primaryColorLight: const Color(0xfff8f6f7),
-      primaryColorDark: const Color(0xff000f4a),
+      primaryColor: const Color(0xffFF4F5B),
+      primaryColorLight: const Color(0xffF4999F),
+      primaryColorDark: const Color(0xffF05454),
       scaffoldBackgroundColor: const Color(0xfff9f9f9),
       iconTheme: IconThemeData(
         color: const Color(0xff0054BF),
@@ -38,13 +38,10 @@ List<Map<String, dynamic>> themes = [
           borderSide: BorderSide.none,
         ),
         hintStyle: TextStyle(
-            fontSize: 13.0,
-            fontWeight: FontWeight.w600,
-            color: const Color(0xff171616).withOpacity(0.45)),
+            fontSize: 13.0, fontWeight: FontWeight.w600, color: const Color(0xff171616).withOpacity(0.45)),
         fillColor: const Color(0xffF4F4F4),
         filled: true,
-        contentPadding:
-            const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
         errorStyle: const TextStyle(
           color: Color(0xffD03333),
           fontSize: 12.0,
@@ -53,17 +50,15 @@ List<Map<String, dynamic>> themes = [
       ),
       cardColor: Colors.white,
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        selectedLabelStyle:
-            const TextStyle(fontSize: 10.0, fontWeight: FontWeight.w800),
-        unselectedLabelStyle:
-            const TextStyle(fontSize: 10.0, fontWeight: FontWeight.w800),
+        selectedLabelStyle: const TextStyle(fontSize: 10.0, fontWeight: FontWeight.w800),
+        unselectedLabelStyle: const TextStyle(fontSize: 10.0, fontWeight: FontWeight.w800),
         type: BottomNavigationBarType.fixed,
         unselectedItemColor: const Color(0xff0054BF).withOpacity(0.35),
         selectedItemColor: const Color(0xff0054BF),
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Color(0xff0054BF)),
+          backgroundColor: MaterialStateProperty.all(Color(0xffFF4F5B)),
         ),
       ),
     ),
@@ -74,12 +69,12 @@ List<Map<String, dynamic>> themes = [
     'brightness': Brightness.dark,
     'data': ThemeData(
       fontFamily: getFontFamilyFromLocale(),
-      brightness: Brightness.dark,
-      primaryColor: const Color(0xff0054BF),
-      primaryColorLight: const Color(0xfff8f6f7),
-      primaryColorDark: const Color(0xff000f4a),
+      primaryColor: const Color(0xffFF4F5B),
+      primaryColorLight: const Color(0xffF4999F),
+      primaryColorDark: const Color(0xffF05454),
+      scaffoldBackgroundColor: const Color(0xfff9f9f9),
       iconTheme: IconThemeData(
-        color: const Color(0xfff8f6f7),
+        color: const Color(0xff0054BF),
       ),
       textTheme: const TextTheme(
         headline1: TextStyle(fontSize: 36.0, fontWeight: FontWeight.w700),
@@ -102,33 +97,27 @@ List<Map<String, dynamic>> themes = [
           borderSide: BorderSide.none,
         ),
         hintStyle: TextStyle(
-            fontSize: 13.0,
-            fontWeight: FontWeight.w600,
-            color: Colors.white.withOpacity(0.45)),
-        fillColor: Colors.grey.shade900,
+            fontSize: 13.0, fontWeight: FontWeight.w600, color: const Color(0xff171616).withOpacity(0.45)),
+        fillColor: const Color(0xffF4F4F4),
         filled: true,
-        contentPadding:
-            const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
+        contentPadding: const EdgeInsets.symmetric(horizontal: 24.0, vertical: 12.0),
         errorStyle: const TextStyle(
           color: Color(0xffD03333),
           fontSize: 12.0,
           fontWeight: FontWeight.w600,
         ),
       ),
-      indicatorColor: const Color(0xff0054BF),
-      cardColor: Colors.black,
+      cardColor: Colors.white,
       bottomNavigationBarTheme: BottomNavigationBarThemeData(
-        selectedLabelStyle:
-            const TextStyle(fontSize: 10.0, fontWeight: FontWeight.w800),
-        unselectedLabelStyle:
-            const TextStyle(fontSize: 10.0, fontWeight: FontWeight.w800),
+        selectedLabelStyle: const TextStyle(fontSize: 10.0, fontWeight: FontWeight.w800),
+        unselectedLabelStyle: const TextStyle(fontSize: 10.0, fontWeight: FontWeight.w800),
         type: BottomNavigationBarType.fixed,
-        unselectedItemColor: const Color(0xfff8f6f7).withOpacity(0.35),
+        unselectedItemColor: const Color(0xff0054BF).withOpacity(0.35),
         selectedItemColor: const Color(0xff0054BF),
       ),
       elevatedButtonTheme: ElevatedButtonThemeData(
         style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all(Color(0xff0054BF)),
+          backgroundColor: MaterialStateProperty.all(Color(0xffFF4F5B)),
         ),
       ),
     ),
@@ -136,16 +125,7 @@ List<Map<String, dynamic>> themes = [
 ];
 
 ThemeData getThemeById(BuildContext context, int id) {
-  return themes.where((element) => element['id'] == id).first['data']
-      as ThemeData;
-  // switch (id) {
-  //   case 1:
-  //   case 2:
-
-  //     return themes.where((element) => element['id']==1).first['data'];
-  //   default:
-  //     return themes['defaultTheme']!;
-  // }
+  return themes.where((element) => element['id'] == id).first['data'] as ThemeData;
 }
 
 String getFontFamilyFromLocale() {
