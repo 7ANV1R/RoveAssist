690efd67947fbb311f284770c17c08d42d7def56
Tanvir <ibnmizan7878@gmail.com>
2021-09-27 10:58:51 +0600

    updated initial page

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -18,7 +18,7 @@ part 'app_routes.dart';
 class AppPages {
   AppPages._();
 
-  static const INITIAL = Routes.NAV_SERVICE;
+  static const INITIAL = Routes.ONBOARDING;
 
   static final routes = [
     GetPage(
