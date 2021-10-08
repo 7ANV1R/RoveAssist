f79c8e384d7e96b645a5bb4dab55b64085c6e60f
Tanvir <ibnmizan7878@gmail.com>
2021-10-09 01:45:34 +0600

    added profile home

--- lib/app/modules/profile/views/profile_view.dart
+++ lib/app/modules/profile/views/profile_view.dart
@@ -1,23 +1,65 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
+import 'package:roveassist/app/widgets/default_button.dart';
 
+import '../../../core/values/assets.dart';
 import '../controllers/profile_controller.dart';
 
 class ProfileView extends GetView<ProfileController> {
   @override
   Widget build(BuildContext context) {
+    final ThemeData _themeData = Theme.of(context);
+    final TextTheme _textTheme = _themeData.textTheme;
+    var screenSize = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('ProfileView'),
-        centerTitle: true,
-      ),
       body: Center(
-        child: Text(
-          'ProfileView is working',
-          style: TextStyle(fontSize: 20),
+          child: Padding(
+        padding: const EdgeInsets.all(36.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset(kAssetOnboarding3),
+            Center(
+              child: Text(
+                'ROVEASSIST',
+                style: _textTheme.headline3!.copyWith(color: _themeData.primaryColor),
+              ),
+            ),
+            SizedBox(
+              height: screenSize.height * 0.01,
+            ),
+            Center(child: Text('Sign up to save your travel related plan')),
+            SizedBox(
+              height: screenSize.height * 0.1,
+            ),
+            DefaultButton(
+              onTap: () {},
+              label: Text(
+                'Login',
+                style: _textTheme.button!.copyWith(color: _themeData.scaffoldBackgroundColor),
+              ),
+              color: _themeData.primaryColor,
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+            ),
+            SizedBox(
+              height: screenSize.height * 0.015,
+            ),
+            DefaultButton(
+              onTap: () {},
+              label: Text(
+                'Sign up',
+                style: _textTheme.button!.copyWith(color: _themeData.primaryColor),
+              ),
+              decoration: BoxDecoration(
+                border: Border.all(color: _themeData.primaryColor, width: 2.0),
+                borderRadius: BorderRadius.circular(100),
+                color: Colors.white,
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 16.0),
+            ),
+          ],
         ),
-      ),
+      )),
     );
   }
 }
---
+++ lib/app/widgets/default_button.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class DefaultButton extends StatelessWidget {
+  const DefaultButton({
+    Key? key,
+    this.decoration,
+    this.color,
+    this.onTap,
+    required this.label,
+    this.highlightColor,
+    this.splashColor,
+    this.padding,
+  }) : super(key: key);
+
+  final BoxDecoration? decoration;
+  final Color? color;
+  final Widget label;
+  final VoidCallback? onTap;
+  final Color? splashColor;
+  final Color? highlightColor;
+  final EdgeInsetsGeometry? padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(100.0),
+      child: Container(
+        height: 44.0,
+        color: color,
+        decoration: decoration,
+        child: Material(
+          color: Colors.transparent,
+          child: InkWell(
+            onTap: onTap,
+            splashColor: splashColor,
+            highlightColor: highlightColor,
+            child: Container(
+              padding: padding,
+              alignment: Alignment.center,
+              child: label,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
